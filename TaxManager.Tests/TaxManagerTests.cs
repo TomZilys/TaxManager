8892c677ebab5e8f806e6c87032d81d113314761
tom.zilys <tom.zilys@gmail.com>
2017-02-05 13:24:27 +0200

    EntityFramerok, Insert method and unit test project added

---
+++ TaxManager.Tests/TaxManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxManager.Models.Enums;
+
+namespace TaxManager.Tests
+{
+    [TestClass]
+    public class TaxManagerTests
+    {
+        [TestMethod]
+        public void TestGetMunicipalityId()
+        {
+            var service = new TaxManagerService();
+            service.InsertMunicipalityTax("testingmunicipality", TaxType.Daily, (decimal)0.08, DateTime.Today, DateTime.Today);
+        }
+    }
+}
--- TaxManager/TaxManager.svc.cs
+++ TaxManager/TaxManager.svc.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using TaxManager.Models;
+using TaxManager.Models.Enums;
 
 namespace TaxManager
 {
@@ -13,6 +16,59 @@ namespace TaxManager
     /// </summary>
     public class TaxManagerService : ITaxManagerService
     {
+        
 
+        public bool InsertMunicipalityTax(string municipalityName, TaxType taxType, decimal taxValue, DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                var municipalityTax = new MunicipalityTax
+                {
+                    MunicipalityId = 1,
+                    TaxTypeId = (int)taxType,
+                    TaxValue = taxValue,
+                    PeriodStartDate = startDate,
+                    PeriodEndDate = endDate
+                };
+
+                using (var taxDbContext = new TaxManagerDBEntities())
+                {
+                    taxDbContext.MunicipalityTax.Add(municipalityTax);
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: log the exception
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns municipalityId by a given municipality name (case insensitive)
+        /// </summary>
+        /// <param name="municipalityName"></param>
+        /// <returns></returns>
+        private int GetMunicipalityId(string municipalityName)
+        {
+            var municipalityId = -1;
+
+            using (var taxDbContext = new TaxManagerDBEntities())
+            {
+                var municipality =
+                    taxDbContext.Municipality.FirstOrDefault(
+                        x => string.Equals(x.MunicipalityName, municipalityName, StringComparison.CurrentCultureIgnoreCase));
+
+                //if (municipality != null)
+                //{
+                //    municipalityId = municipality.Id;
+                //}
+
+                municipalityId = municipality?.Id ?? -1;
+            }
+
+            return municipalityId;
+        }
     }
 }

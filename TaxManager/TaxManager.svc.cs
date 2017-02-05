439aaf298f91562aa8514f91866cdc10b8f8eeef
tom.zilys <tom.zilys@gmail.com>
2017-02-05 11:42:06 +0200

    Initial commit

---
+++ TaxManager/TaxManager.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TaxManager
+{
+    /// <summary>
+    /// The main service class that is responsible for managing municipality taxes
+    /// </summary>
+    public class TaxManagerService : ITaxManagerService
+    {
+
+    }
+}
